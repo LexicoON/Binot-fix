b32773c19ef09549016cede7ba8bc66a8ad8af6c
LexicoON <140135754+LexicoON@users.noreply.github.com>
2026-09-06 16:17:34 -0500

    Bump version code to 15 and version name to 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.binot.xrztp"
     minSdk = 24
     targetSdk = 36
-    versionCode = 14 
-    versionName = "1.1.2"
+    versionCode = 15 
+    versionName = "1.1.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
