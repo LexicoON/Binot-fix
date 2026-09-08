fb6f4fdcea1a8472099efd741ba327770b5b289c
LexicoON <140135754+LexicoON@users.noreply.github.com>
2026-09-07 20:28:52 -0500

    Update versionCode and versionName to 17 and 1.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.binot.xrztp"
     minSdk = 24
     targetSdk = 36
-    versionCode = 16 
-    versionName = "1.1.4"
+    versionCode = 17 
+    versionName = "1.1.5"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
